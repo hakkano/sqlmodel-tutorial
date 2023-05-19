512012707084e287d138d3bffe7d624f21dc5f6f
hakkano <cool.sage.1@gmail.com>
2023-05-19 13:05:09 +0530

    added pytest fixtures to avoid duplication

--- test_main.py
+++ test_main.py
@@ -1,33 +1,39 @@
+import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 
 from .main import app, get_session
 
-
-def test_create_hero():
-        engine = create_engine(
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
                 "sqlite://", connect_args={"check_same_thread": False},
                 poolclass=StaticPool
         )
-        SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
 
-        with Session(engine) as session:
-            def get_session_override():
+    with Session(engine) as session:
+           yield session
+      
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+        def get_session_override():
                 return session
         
-            app.dependency_overrides[get_session] = get_session_override
+        app.dependency_overrides[get_session] = get_session_override
+        client = TestClient(app) 
+        yield client
+        app.dependency_overrides.clear()
+       
 
-            client = TestClient(app) 
-
-            response = client.post(
-                "/heroes/", json={"name": "Deadpond", "secret_name": "Dive Wilson"}
-            )
-            app.dependency_overrides.clear()
-            data = response.json()
-
-            assert response.status_code == 200
-            assert data["name"] == "Deadpond" 
-            assert data["secret_name"] == "Dive Wilson"
-            assert data["age"] is None
-            assert data["id"] is not None
+def test_create_hero(client: TestClient):
+    response = client.post("/heroes/", json={"name": "Deadpond", "secret_name": "Dive Wilson"})         
+    data = response.json()
+    
+    assert response.status_code == 200
+    assert data["name"] == "Deadpond" 
+    assert data["secret_name"] == "Dive Wilson"
+    assert data["age"] is None
+    assert data["id"] is not None
